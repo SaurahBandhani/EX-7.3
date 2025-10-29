fde0979b3ad668be48090f9e5f1ed8ac963d638d
Hitesh Sharma <hiteshsharma66018@gmail.com>
2025-10-29 15:49:05 +0530

    Initialize Express server with Socket.io support

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Receive message and broadcast to all
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
